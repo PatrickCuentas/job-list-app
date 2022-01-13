b3bcbb68b8fe3e4db5662d028c384206ea955e5c
patrick_cuentas <patrickcuentasmariano@gmail.com>
2022-01-13 12:47:24 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      blue: "#1fb6ff",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      "desaturated-dark-cyan": "hsl(180,29%,50%)",
+      "light-grayish-cyan-background": "hsl(180,52%,96%)",
+      "light-grayish-cyan-tablet": "hsl(180,31%,95%)",
+      "dark-grayish-cyan": "hsl(180,8%,52%)",
+      "very-dark-grayish-cyan": "hsl(180,14%,20%)",
+    },
+    fontFamily: {
+      spartan: ["Spartan", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
